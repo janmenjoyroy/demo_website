7ed4155dd5966bb42d3cbfc27d9e5b24c491a445
Janmenjoy Roy <janmenjoyroy@yahoo.co.in>
2017-02-07 22:17:34 +0530

    Added page switching

--- server.js
+++ server.js
@@ -1,19 +1,49 @@
 var express = require('express')
   , logger = require('morgan')
   , app = express()
-  , template = require('jade').compileFile(__dirname + '/source/templates/homepage.jade')
 
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
 
 app.get('/', function (req, res, next) {
   try {
+   var template = require('jade').compileFile(__dirname + '/source/templates/home.jade')
     var html = template({ title: 'Home' })
     res.send(html)
   } catch (e) {
     next(e)
   }
-})
+});
+
+app.get('/news', function (req, res, next) {
+  try {
+   var template = require('jade').compileFile(__dirname + '/source/templates/news.jade')
+    var html = template({ title: 'News' })
+    res.send(html)
+  } catch (e) {
+    next(e)
+  }
+});
+
+app.get('/about', function (req, res, next) {
+  try {
+   var template = require('jade').compileFile(__dirname + '/source/templates/about.jade')
+    var html = template({ title: 'About' })
+    res.send(html)
+  } catch (e) {
+    next(e)
+  }
+});
+
+app.get('/contact', function (req, res, next) {
+  try {
+   var template = require('jade').compileFile(__dirname + '/source/templates/contact.jade')
+    var html = template({ title: 'Contact' })
+    res.send(html)
+  } catch (e) {
+    next(e)
+  }
+});
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
